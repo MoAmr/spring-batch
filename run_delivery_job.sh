0c9be3574c7fbf87698a329045d3da77b0395ff1
Mohammed Amr <mohammed.amr.kassim@gmail.com>
2022-11-05 23:19:51 +0300

    Job Flow. closes #9

--- run_delivery_job.sh
+++ run_delivery_job.sh
@@ -2,5 +2,5 @@
 CURRENT_DATE=`date '+%Y/%m/%d'`
 LESSON=$(basename $PWD)
 mvn clean package -Dmaven.test.skip=true;
-java -jar ./target/spring-batch-0.0.1-SNAPSHOT.jar "item=shoes" "run.date(date)=$CURRENT_DATE" "lesson=$LESSON";
+java -jar ./target/spring-batch-0.0.1-SNAPSHOT.jar "item=shoes_$CURRENT_DATE" "run.date(date)=$CURRENT_DATE" "lesson=$LESSON";
 read;
